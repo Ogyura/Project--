db4ae04d2a52b231c459588ff8a54c8fc87e84d5
Ogyura <ogannisan00@bk.ru>
2025-12-13 12:13:43 +0300

    Update pz 5.2.py

--- PZ 5/pz 5.2.py
+++ PZ 5/pz 5.2.py
@@ -1,7 +1,9 @@
 def sum_series(n):
     result = 0
-    for i in range(1, n + 1):
+    i = 1
+    while i <= n:
         result += i
+        i += 1
     return result
     
 def sort_dec3(a, b, c):
@@ -13,16 +15,17 @@ def sort_dec3(a, b, c):
         b, c = c, b
     return a, b, c
 
-print("N = ", end="")
-n = int(input()) 
+n = int(input())
 
-print("Сумма ряда:", sum_series(n))
+result = sum_series(n)
 
-print("Введите 3 числа (каждое с новой строки):")
-x = float(input())  
-y = float(input())   
-z = float(input())  
+x = float(input())
+y = float(input())
+z = float(input())
 
 x, y, z = sort_dec3(x, y, z)
-print("После сортировки:", x, y, z)
 
+print(result)
+print(x)
+print(y)
+print(z)
